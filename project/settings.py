fa804d381160e3200610a5aabb5b5a8674cf18a1
pedro-coelho-dr <pcdr@cesar.school>
2023-10-29 19:55:13 -0300

    small

--- project/settings.py
+++ project/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Feel free to alter this value to suit your needs.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        default='postgresql://postgres:postgres@localhost:5432/libresenses',
         conn_max_age=600
     )
 }
