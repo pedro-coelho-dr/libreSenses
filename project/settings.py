9998a69fdbe15693e7c8a0515b3db81858cb5dbc
pedro-coelho-dr <pcdr@cesar.school>
2023-10-29 23:32:46 -0300

    render #3

--- project/settings.py
+++ project/settings.py
@@ -100,16 +100,21 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     #####REMOVE_FOR_RENDER
-    """ 'default': {
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    } """
+    }        
+} """
+    
     #####RENDER
-        'default': dj_database_url.parse(
-            os.environ.get('DATABASE_URL'), 
-            conn_max_age=600),
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ['DATABASE_URL'],
+        conn_max_age=600
+    )
 }
 
 
