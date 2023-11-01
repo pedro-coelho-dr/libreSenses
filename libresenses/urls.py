12dfe9202f943ad1508a582d36f7f407597e167c
pedro-coelho-dr <pcdr@cesar.school>
2023-11-01 17:51:51 -0300

    converti os views de function based views para class based views, está mais oop.

--- libresenses/urls.py
+++ libresenses/urls.py
@@ -5,19 +5,20 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('submit_film/', views.submit_film, name='submit_film'),  # Changed URL pattern here
-    path('add_caption/<int:film_id>/', views.add_caption, name='add_caption'),
-    path('film/<int:film_id>/', views.getFilm, name='film_profile'),
-    path('film/update/<int:film_id>/', views.update_film, name='update_film'),
-    path('update_caption/<int:caption_id>/', views.update_caption, name='update_caption'),
-    path('add_audio_description/<int:film_id>/', views.add_audio_description, name='add_audio_description'),
-    path('update_audio_description/<int:audio_description_id>/', views.update_audio_description, name='update_audio_description'),
-    path('add_sign_language/<int:film_id>/', views.add_sign_language, name='add_sign_language'),
-    path('update_sign_language/<int:sign_language_id>/', views.update_sign_language, name='update_sign_language'),
-    path('add_media_alternative/<int:film_id>/', views.add_media_alternative, name='add_media_alternative'),
-    path('update_media_alternative/<int:media_alternative_id>/', views.update_media_alternative, name='update_media_alternative'),
+    path('', views.Index.as_view(), name='index'),
+    path('submit_film/', views.SubmitFilm.as_view(), name='submit_film'),
+    path('add_caption/<int:film_id>/', views.AddCaption.as_view(), name='add_caption'),
+    path('film/<int:film_id>/', views.FilmProfile.as_view(), name='film_profile'),
+    path('film/update/<int:film_id>/', views.UpdateFilm.as_view(), name='update_film'),
+    path('update_caption/<int:caption_id>/', views.UpdateCaption.as_view(), name='update_caption'),
+    path('add_audio_description/<int:film_id>/', views.AddAudioDescription.as_view(), name='add_audio_description'),
+    path('update_audio_description/<int:audio_description_id>/', views.UpdateAudioDescription.as_view(), name='update_audio_description'),
+    path('add_sign_language/<int:film_id>/', views.AddSignLanguage.as_view(), name='add_sign_language'),
+    path('update_sign_language/<int:sign_language_id>/', views.UpdateSignLanguage.as_view(), name='update_sign_language'),
+    path('add_media_alternative/<int:film_id>/', views.AddMediaAlternative.as_view(), name='add_media_alternative'),
+    path('update_media_alternative/<int:media_alternative_id>/', views.UpdateMediaAlternative.as_view(), name='update_media_alternative'),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- libresenses/views.py
+++ libresenses/views.py
@@ -1,190 +1,194 @@
+from django.views import View
 from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseBadRequest
+from .models import Film, Caption, AudioDescription, SignLanguage, MediaAlternative
 from .forms import (
     FilmForm, CaptionForm, AudioDescriptionForm, 
     SignLanguageForm, MediaAlternativeForm
 )
 
-from django.http import HttpResponseBadRequest
-from .models import Film, Caption, AudioDescription, SignLanguage, MediaAlternative
+# Index View
+class Index(View):
+    def get(self, request):
+        films = Film.objects.all()
+        return render(request, 'index.html', {'films': films})
 
-def index(request):
-    films = Film.objects.all()
-    return render(request, 'index.html', {'films': films})
 
-# Film Profile
+# Film Profile View
+class FilmProfile(View):
+    def get(self, request, film_id):
+        film = get_object_or_404(Film, pk=film_id)
+        
+        captions = film.captions.all()
+        audio_descriptions = film.audio_descriptions.all()
+        sign_languages = film.sign_languages.all()
+        media_alternatives = film.media_alternatives.all()
 
-def getFilm(request, film_id):
-    film = get_object_or_404(Film, pk=film_id)
-    
-    captions = film.captions.all()
-    audio_descriptions = film.audio_descriptions.all()
-    sign_languages = film.sign_languages.all()
-    media_alternatives = film.media_alternatives.all()
+        caption_form = CaptionForm(initial={'film': film})
+        audio_description_form = AudioDescriptionForm(initial={'film': film})
+        sign_language_form = SignLanguageForm(initial={'film': film})
+        media_alternative_form = MediaAlternativeForm(initial={'film': film})
 
-    caption_form = CaptionForm(initial={'film': film})
-    audio_description_form = AudioDescriptionForm(initial={'film': film})
-    sign_language_form = SignLanguageForm(initial={'film': film})
-    media_alternative_form = MediaAlternativeForm(initial={'film': film})
+        context = {
+            'film': film, 
+            'captions': captions, 
+            'audio_descriptions': audio_descriptions,
+            'sign_languages': sign_languages,
+            'media_alternatives': media_alternatives,
+            'caption_form': caption_form,
+            'audio_description_form': audio_description_form,
+            'sign_language_form': sign_language_form,
+            'media_alternative_form': media_alternative_form
+        }
 
-    context = {
-        'film': film, 
-        'captions': captions, 
-        'audio_descriptions': audio_descriptions,
-        'sign_languages': sign_languages,
-        'media_alternatives': media_alternatives,
-        'caption_form': caption_form,
-        'audio_description_form': audio_description_form,
-        'sign_language_form': sign_language_form,
-        'media_alternative_form': media_alternative_form
-    }
+        return render(request, 'film_profile.html', context)
 
-    return render(request, 'film_profile.html', context)
+# Submit Film View
 
-# Film
+class SubmitFilm(View):
+    def get(self, request):
+        film_form = FilmForm()
+        return render(request, 'submit_film.html', {'film_form': film_form})
 
-def submit_film(request):
-    if request.method == 'POST':
+    def post(self, request):
         film_form = FilmForm(request.POST, request.FILES)
         if film_form.is_valid():
             film = film_form.save()
             return redirect('film_profile', film_id=film.id)
-    else:
-        film_form = FilmForm()
+        return render(request, 'submit_film.html', {'film_form': film_form})
 
-    context = {'film_form': film_form}
-    return render(request, 'submit_film.html', context)
+# Update Film View
+class UpdateFilm(View):
+    def get(self, request, film_id):
+        film = get_object_or_404(Film, pk=film_id)
+        form = FilmForm(instance=film)
+        return render(request, 'update_film.html', {'form': form})
 
-def update_film(request, film_id):
-    film = get_object_or_404(Film, pk=film_id)
-    if request.method == "POST":
+    def post(self, request, film_id):
+        film = get_object_or_404(Film, pk=film_id)
         form = FilmForm(request.POST, request.FILES, instance=film)
         if form.is_valid():
             form.save()
             return redirect('film_profile', film_id=film.id)
-        else:
-            return HttpResponseBadRequest("Invalid data")
-    return redirect('getFilm', film_id=film.id)
+        return HttpResponseBadRequest("Invalid data")
 
-# Caption
-
-def add_caption(request, film_id):
-    film = get_object_or_404(Film, pk=film_id)
-    
-    if request.method == 'POST':
+# Add Caption View
+class AddCaption(View):
+    def post(self, request, film_id):
+        film = get_object_or_404(Film, pk=film_id)
         form = CaptionForm(request.POST, request.FILES)
-        
         if form.is_valid():
             caption = form.save(commit=False)
             caption.film = film
             caption.save()
             return redirect('film_profile', film_id=film_id)
-        
         return redirect('film_profile', film_id=film_id)
-    
-def update_caption(request, caption_id):
-    caption = get_object_or_404(Caption, pk=caption_id)
-    film = caption.film
 
-    if request.method == 'POST':
+# Update Caption View
+class UpdateCaption(View):
+    def get(self, request, caption_id):
+        caption = get_object_or_404(Caption, pk=caption_id)
+        film = caption.film
+        form = CaptionForm(instance=caption)
+        context = {'caption_form': form, 'film': film, 'edit_mode': True}
+        return render(request, 'film_profile.html', context)
+
+    def post(self, request, caption_id):
+        caption = get_object_or_404(Caption, pk=caption_id)
+        film = caption.film
         form = CaptionForm(request.POST, request.FILES, instance=caption)
         if form.is_valid():
             form.save()
             return redirect('film_profile', film_id=film.id)
-    else:
-        form = CaptionForm(instance=caption)
+        context = {'caption_form': form, 'film': film, 'edit_mode': True}
+        return render(request, 'film_profile.html', context)
 
-    context = {'caption_form': form, 'film': film, 'edit_mode': True}
-    return render(request, 'film_profile.html', context)
-
-#Audio Description
-
-def add_audio_description(request, film_id):
-    film = get_object_or_404(Film, pk=film_id)
-    
-    if request.method == 'POST':
+# Audio Description Views
+class AddAudioDescription(View):
+    def post(self, request, film_id):
+        film = get_object_or_404(Film, pk=film_id)
         form = AudioDescriptionForm(request.POST, request.FILES)
-        
         if form.is_valid():
-            audio_description = form.save(commit=False)  
-            audio_description.film = film  
-            audio_description.save()  
+            audio_description = form.save(commit=False)
+            audio_description.film = film
+            audio_description.save()
             return redirect('film_profile', film_id=film_id)
-        
         return redirect('film_profile', film_id=film_id)
 
-def update_audio_description(request, audio_description_id):
-    audio_description = get_object_or_404(AudioDescription, pk=audio_description_id)
-    film = audio_description.film
+class UpdateAudioDescription(View):
+    def get(self, request, audio_description_id):
+        audio_description = get_object_or_404(AudioDescription, pk=audio_description_id)
+        film = audio_description.film
+        form = AudioDescriptionForm(instance=audio_description)
+        context = {'audio_description_form': form, 'film': film, 'edit_mode': True}
+        return render(request, 'film_profile.html', context)
 
-    if request.method == 'POST':
+    def post(self, request, audio_description_id):
+        audio_description = get_object_or_404(AudioDescription, pk=audio_description_id)
+        film = audio_description.film
         form = AudioDescriptionForm(request.POST, request.FILES, instance=audio_description)
         if form.is_valid():
             form.save()
             return redirect('film_profile', film_id=film.id)
-    else:
-        form = AudioDescriptionForm(instance=audio_description)
+        context = {'audio_description_form': form, 'film': film, 'edit_mode': True}
+        return render(request, 'film_profile.html', context)
 
-    context = {'audio_description_form': form, 'film': film, 'edit_mode': True}
-    return render(request, 'film_profile.html', context)
-
-# Sign Language
-
-def add_sign_language(request, film_id):
-    film = get_object_or_404(Film, pk=film_id)
-    
-    if request.method == 'POST':
+# Sign Language Views
+class AddSignLanguage(View):
+    def post(self, request, film_id):
+        film = get_object_or_404(Film, pk=film_id)
         form = SignLanguageForm(request.POST, request.FILES)
-        
         if form.is_valid():
-            sign_language = form.save(commit=False)  
-            sign_language.film = film  
-            sign_language.save()  
+            sign_language = form.save(commit=False)
+            sign_language.film = film
+            sign_language.save()
             return redirect('film_profile', film_id=film_id)
-        
         return redirect('film_profile', film_id=film_id)
 
-def update_sign_language(request, sign_language_id):
-    sign_language = get_object_or_404(SignLanguage, pk=sign_language_id)
-    film = sign_language.film
+class UpdateSignLanguage(View):
+    def get(self, request, sign_language_id):
+        sign_language = get_object_or_404(SignLanguage, pk=sign_language_id)
+        film = sign_language.film
+        form = SignLanguageForm(instance=sign_language)
+        context = {'sign_language_form': form, 'film': film, 'edit_mode': True}
+        return render(request, 'film_profile.html', context)
 
-    if request.method == 'POST':
+    def post(self, request, sign_language_id):
+        sign_language = get_object_or_404(SignLanguage, pk=sign_language_id)
+        film = sign_language.film
         form = SignLanguageForm(request.POST, request.FILES, instance=sign_language)
         if form.is_valid():
             form.save()
             return redirect('film_profile', film_id=film.id)
-    else:
-        form = SignLanguageForm(instance=sign_language)
+        context = {'sign_language_form': form, 'film': film, 'edit_mode': True}
+        return render(request, 'film_profile.html', context)
 
-    context = {'sign_language_form': form, 'film': film, 'edit_mode': True}
-    return render(request, 'film_profile.html', context)
-
-# Media Alternative
-
-def add_media_alternative(request, film_id):
-    film = get_object_or_404(Film, pk=film_id)
-    
-    if request.method == 'POST':
+# Media Alternative Views
+class AddMediaAlternative(View):
+    def post(self, request, film_id):
+        film = get_object_or_404(Film, pk=film_id)
         form = MediaAlternativeForm(request.POST, request.FILES)
-        
         if form.is_valid():
-            media_alternative = form.save(commit=False)  
-            media_alternative.film = film  
-            media_alternative.save()  
+            media_alternative = form.save(commit=False)
+            media_alternative.film = film
+            media_alternative.save()
             return redirect('film_profile', film_id=film_id)
-        
         return redirect('film_profile', film_id=film_id)
 
-def update_media_alternative(request, media_alternative_id):
-    media_alternative = get_object_or_404(MediaAlternative, pk=media_alternative_id)
-    film = media_alternative.film
+class UpdateMediaAlternative(View):
+    def get(self, request, media_alternative_id):
+        media_alternative = get_object_or_404(MediaAlternative, pk=media_alternative_id)
+        film = media_alternative.film
+        form = MediaAlternativeForm(instance=media_alternative)
+        context = {'media_alternative_form': form, 'film': film, 'edit_mode': True}
+        return render(request, 'film_profile.html', context)
 
-    if request.method == 'POST':
+    def post(self, request, media_alternative_id):
+        media_alternative = get_object_or_404(MediaAlternative, pk=media_alternative_id)
+        film = media_alternative.film
         form = MediaAlternativeForm(request.POST, request.FILES, instance=media_alternative)
         if form.is_valid():
             form.save()
             return redirect('film_profile', film_id=film.id)
-    else:
-        form = MediaAlternativeForm(instance=media_alternative)
-
-    context = {'media_alternative_form': form, 'film': film, 'edit_mode': True}
-    return render(request, 'film_profile.html', context)
+        context = {'media_alternative_form': form, 'film': film, 'edit_mode': True}
+        return render(request, 'film_profile.html', context)
